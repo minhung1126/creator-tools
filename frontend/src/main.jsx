9b808b41be1dbf8a95c72a2cac792d3cdb2c9440
minhung1126 <93239260+minhung1126@users.noreply.github.com>
2026-08-01 05:35:02 +0800

    Load notification center styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './sticky-bulk-edit.css'
+import './notification-center.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
